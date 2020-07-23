f81949700f799f21f5d8133c1b5af344e94d205a
Alozie Nwosu <alozie@gmail.com>
2020-07-22 22:50:23 -0400

    AS220-000: Updates all system components (BLT, Drush, Drupal,...).

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -765,3 +765,11 @@ $settings['file_scan_ignore_directories'] = [
 # }
 require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
 $settings['install_profile'] = 'standard';
+/**
+ * IMPORTANT.
+ *
+ * Do not include additional settings here. Instead, add them to settings
+ * included by `blt.settings.php`. See BLT's documentation for more detail.
+ *
+ * @link https://docs.acquia.com/blt/
+ */
